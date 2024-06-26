783b4b11cb4aba3c6ee6131f96326cea5937a6f1
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2024-06-26 21:58:25 +0300

    update about me and removing ASH from projects json

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -24,15 +24,16 @@ const About = () => {
       <article className="prose prose-p:text-foreground" aria-label="about me">
         <p>
           I'm a web developer with a knack for building cool things on the internet.
+          I'm always seeking new ways to enhance my skills and the projects I work
+          on.
         </p>
 
         <p>
-          I'm always seeking new ways to enhance my skills and the projects I work
-          on. My journey started back in 2021 when I decided to learn HTML/CSS in my
-          free time just for fun. I never thought I would get serious about it. I
-          just wanted to build basic web pages and see how it's done out of
-          curiosity. I had a lot of fun, and a few months later I decided to step up
-          my game and learn JavaScript.
+          My journey started back in 2021 when I decided to learn HTML/CSS in my free
+          time just for fun. I never thought I would get serious about it. I just
+          wanted to build basic web pages and see how it's done out of curiosity. I
+          had a lot of fun, and a few months later I decided to step up my game and
+          learn JavaScript.
         </p>
 
         <p>
