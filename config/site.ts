f9e9f2b087bb01de9f741bc6cc9fa6d5f9501974
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2024-07-13 02:01:29 +0300

    updating description

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "webdevkalo",
-  description: "Make beautiful websites regardless of your design experience.",
+  description: "",
   navItems: [
     {
       label: "Projects",
